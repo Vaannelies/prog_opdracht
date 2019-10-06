79a8f1631239314b02b5330ea16ad8956bfa5faa
Vaannelies <0962949@hr.nl>
2019-10-06 15:58:13 +0200

    Na ingelogd (admin) direct op keuzescherm
    Eerst kwam je dan op een homepagina waar alleen stond dat je was ingelogd, waarna je naar de select.blade.php pagina kon gaan via dat menu. Ik ga nu select.blade.php weghalen. De content van die pagina staat nu direct op deze home pagina.

--- OEFENEN/CRUDTryOut/resources/views/home.blade.php
+++ OEFENEN/CRUDTryOut/resources/views/home.blade.php
@@ -1,23 +1,42 @@
-@extends('layouts.app')
+
+@extends('layouts.adminstyle');
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Select</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
+
                     @endif
 
-                    You are logged in!
+                        <div class="links">
+                            <a href="{{route('animals.create')}}"><button class="button">Employees</button></a>
+                            <a href=""><button class="button">Animals</button></a>
+                        </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+@endsection
+
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        
+           
+               
+                </div>
+            </div>
+        
     </div>
 </div>
 @endsection
