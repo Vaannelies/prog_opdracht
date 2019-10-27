9bcd0f27ad35a6b6fc7fdf98e0bdd5f574403175
Vaannelies <0962949@hr.nl>
2019-10-27 18:52:31 +0100

    employees table meer kolommen en admin (users) seeder gemaakt
    werkt!

--- OEFENEN/CRUDTryOut/database/seeds/DatabaseSeeder.php
+++ OEFENEN/CRUDTryOut/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(SpeciesTableSeeder::class);
         $this->call(AnimalsTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
