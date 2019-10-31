87031f4fec9a201fe12f7a2ed39a078409b99ff8
Vaannelies <0962949@hr.nl>
2019-10-31 15:45:58 +0100

    HOME PAGINA REAGEERT OP ROLES BIJ INLOGGEN

--- OEFENEN/CRUDTryOut/resources/views/home.blade.php
+++ OEFENEN/CRUDTryOut/resources/views/home.blade.php
@@ -1,44 +1,70 @@
+@can('login-admins')
 
-@extends('layouts.adminstyle')
+    @extends('layouts.adminstyle')
 
-@section('content')
-<title>Admin Page</title>
+    @section('content')
+    <title>Admin Page</title>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Select</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Select</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
 
-                    @endif
+                        @endif
 
-                        <div class="links">
-                            <a href="{{route('admin.employees.index')}}"><button class="button">Employees</button></a>
-                            <a href="{{route('admin.animals.index')}}"><button class="button">Animals</button></a>
-                        </div>
+                            <div class="links">
+                                <a href="{{route('admin.employees.index')}}"><button class="button">Employees</button></a>
+                                <a href="{{route('admin.animals.index')}}"><button class="button">Animals</button></a>
+                            </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
+@endcan
 
+@can('login-employees')
+    @extends('layouts.employeestyle')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        
-           
-               
+    @section('content')
+    <title>Employee Page</title>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+            <p>Hello [[dit werkt nog niet]]!</p>
+            
+                <div class="card">
+                    <div class="card-header">Select</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+
+                        @endif
+
+                            <div class="links">
+                                <a href="{{route('employee.dashboard')}}"><button class="button">You</button></a>
+                                <a href="{{route('employee.dashboard')}}"><button class="button">Animals</button></a>
+                            </div>
+                    </div>
                 </div>
             </div>
-        
+        </div>
     </div>
-</div>
-@endsection
+    @endsection
+@endcan
+
+
+
